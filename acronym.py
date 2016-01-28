e33fc6b8e2f349a9da97e7f3d2673f7458f09c31
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2016-01-28 16:01:58 -0600

    Add acronym module.

---
+++ acronym.py
@@ -0,0 +1,16 @@
+"""Take input from a user and print the corresponding acronym."""
+
+from underscores import get_input
+
+def generate_acronym(string):
+    """Create an acronym from a given string input and return it."""
+    words = string.split()
+    letters = []
+    for word in words:
+        letters.append(word[0])
+    acronym = ''.join(letters)
+    return acronym.upper()
+
+if __name__ == "__main__":
+    INPUT = get_input()
+    print generate_acronym(INPUT)
--- underscores.py
+++ underscores.py
@@ -5,11 +5,11 @@ def get_input():
     """Propmt the user for input and store it as a string."""
     return raw_input("Enter whatever your heart desires:")
 
-def replace_space(input):
+def replace_space(string):
     """Replace all the whitespace found in a given string with an
     underscore and return it.
     """
-    words_list = input.split()
+    words_list = string.split()
     return '_'.join(words_list)
 
 if __name__ == "__main__":
